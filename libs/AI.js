46141c7c95e004a380a99b8eb6890c37cc4b3c71
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2022-12-22 09:21:06 +0100

    added meaningful comments

--- libs/AI.js
+++ libs/AI.js
@@ -1,22 +1,36 @@
 import { Configuration, OpenAIApi } from "openai"
 import dialog from '../dialog.json' assert { type: "json" }
 
+// Configuration object to authenticate with the OpenAI API
 const config = new Configuration({ apiKey: process.env.OPENAI_KEY })
+
+// Create an instance of the OpenAI API using the configuration object
 const openai = new OpenAIApi(config)
 
+// Export the compose function, which uses the OpenAI API to generate text completions
 export const compose = async function (content) {
+    // Asynchronously generate a text completion using the OpenAI API
     const gen = async prompt =>
         await openai.createCompletion({
+            // Use the text-davinci-003 model to generate completions
             model: "text-davinci-003",
+            // Use a temperature of 0.9 to encourage more diverse completions
             temperature: 0.9,
+            // Generate completions with a maximum of 150 tokens
             max_tokens: 150,
+            // Use the top_p parameter to ensure the most likely completions are returned
             top_p: 1,
+            // Provide the prompt for the completion
             prompt,
+            // Set the frequency penalty to 0 to not penalize common words
             frequency_penalty: 0,
+            // Set the presence penalty to 0.6 to encourage the model to generate novel text
             presence_penalty: 0.6,
+            // Use the "Human:" and "AI:" strings to indicate the end of the completion
             stop: [" Human:", " AI:"]
         })
 
+    // Return the generated subject and body of the email as an object
     return {
         subject: ('"' + (await gen(`${dialog.subject}:\n\n"${content}"\n\nAI: "`)).data.choices[0].text).replace(/(^"|"$)/g, ''),
         body: ('"' + (await gen(`${dialog.body}:\n\n"${content}"\n\nAI:`)).data.choices[0].text).replace(/(^"|"$)/g, '')
--- libs/IMAP.js
+++ libs/IMAP.js
@@ -2,6 +2,7 @@ import Imap from 'imap'
 import mailer from 'nodemailer'
 import { simpleParser } from 'mailparser'
 
+// Connect to an IMAP server
 const compose = new Imap({
     user: process.env.IMAP_USER,
     password: process.env.IMAP_PASSWORD,
@@ -10,6 +11,7 @@ const compose = new Imap({
     tls: true
 })
 
+// Connect to an SMTP server
 const sender = mailer.createTransport({
     host: process.env.IMAP_HOST,
     port: 465,
@@ -20,17 +22,25 @@ const sender = mailer.createTransport({
     }
 })
 
+// Asynchronously fetch and parse the contents of unread emails in the INBOX of an IMAP account
 export const fetch = async () => new Promise(async res => {
+    // Connect to the IMAP server
     compose.connect()
 
+    // Wait until the connection is ready
     await compose.once('ready', () => {
+        // Open the INBOX folder
         compose.openBox('INBOX', false, async err => {
+            // Search for unread emails sent to the specified recipient
             compose.search(['UNSEEN', ['TO', process.env.IMAP_RECIEVER]], (err, results) => {
+                // Fetch the contents of the found emails
                 let inbox = compose.fetch(results, { bodies: '', markSeen: true, struct: true })
 
-                // compose.setFlags(results, ['\\Seen'])
+                // Mark E-mails as read, to filter them out for next iteration.
+                compose.setFlags(results, ['\\Seen'])
 
                 let collect = []
+                // Process the contents of each email
                 inbox.on('message', m => {
                     m.on('body', stream => {
                         simpleParser(stream, (err, { from, text }) => collect.push({
@@ -40,8 +50,10 @@ export const fetch = async () => new Promise(async res => {
                     })
                 })
 
+                // Wait two seconds and resolve the promise with the collected emails
                 setTimeout(() => {
                     res(collect ? collect : [])
+                    // Close the connection to the IMAP server
                     compose.end()
                 }, 2000)
             })
@@ -49,7 +61,9 @@ export const fetch = async () => new Promise(async res => {
     })
 })
 
-export const send = async ({ from, to, subject, text }) => {
+// Asynchronously send an email via an SMTP server
+export const send = async ({ to, subject, text }) => {
+    // Send the email using the SMTP transport
     await sender.sendMail({
         from: process.env.IMAP_RECIEVER,
         to, subject, text
